513b1b712be781b0619a47cb752367883c597b4b
tofik-93 <tofikmifta87@gmail.com>
2026-02-23 11:42:32 +0300

    feat: update Next.js and React dependencies in package.json and pnpm-lock.yaml

--- apps/admin/next.config.ts
+++ apps/admin/next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    turbopack: {
+      // This tells Turbopack to look up two levels (the root of the repo)
+      root: '../../', 
+    },
+  },
 };
 
 export default nextConfig;
